f8072b5f0861cb4418bca5cbbd1b91e63b91a166
CodeWithRJ <13061978+code-with-rj1399@users.noreply.github.com>
2026-09-18 21:23:41 +0530

    fix: tolerate unknown DynamoDB entity fields

--- src/main/java/ai/interviewhq/crawler/domain/CrawlPage.java
+++ src/main/java/ai/interviewhq/crawler/domain/CrawlPage.java
@@ -1,7 +1,10 @@
 package ai.interviewhq.crawler.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 
 import java.time.Instant;
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CrawlPage {
     private Integer id;
 
