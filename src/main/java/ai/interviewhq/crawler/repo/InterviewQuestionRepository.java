175ec367295bfab8c195a7d609405a09162bd7a7
CodeWithRJ <13061978+code-with-rj1399@users.noreply.github.com>
2026-09-26 01:37:34 +0530

    feat: store interview questions in dedicated table

--- src/main/java/ai/interviewhq/crawler/repo/InterviewQuestionRepository.java
+++ src/main/java/ai/interviewhq/crawler/repo/InterviewQuestionRepository.java
@@ -4,6 +4,7 @@ import ai.interviewhq.crawler.config.DynamoDbRepositorySupport;
 import ai.interviewhq.crawler.domain.InterviewQuestion;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 import java.util.Comparator;
@@ -14,7 +15,8 @@ import java.util.Optional;
 public class InterviewQuestionRepository extends DynamoRepository<InterviewQuestion, Integer> {
     private static final Logger log = LoggerFactory.getLogger(InterviewQuestionRepository.class);
 
-    public InterviewQuestionRepository(DynamoDbRepositorySupport db) {
+    public InterviewQuestionRepository(
+            @Qualifier("questionDynamoDbRepositorySupport") DynamoDbRepositorySupport db) {
         super(db);
     }
 
